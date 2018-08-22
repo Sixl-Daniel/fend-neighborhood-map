6d8ef65f94752b579f2564fa0b7967827d75224b
Sixl-Daniel <sixl.daniel@googlemail.com>
2018-08-22 04:41:17 +0200

    feat: Activate service worker

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 import './semantic/dist/semantic.min.css';
 import App from './App';
-// import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
+registerServiceWorker();
